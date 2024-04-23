642da3fdf3efc3518d5d6233f5e3b9744be3eed3
chamika-damith <chamikadamith9@gmail.com>
2024-04-23 12:11:49 +0530

    add javascript project section

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const desktopLink = document.querySelector(".desktop-link");
+    const webLink=document.querySelector(".web-link");
+    const allLinks = document.querySelectorAll(".all-link");
     const desktopProjects = document.querySelectorAll(".desktop-project");
     const webProject = document.querySelector(".web-project");
 
@@ -9,5 +11,23 @@ document.addEventListener("DOMContentLoaded", function() {
             project.style.display = "block";
         });
         webProject.style.display = "none";
+    });
+
+    webLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        desktopProjects.forEach(function(project) {
+            project.style.display = "none";
+        });
+        webProject.style.display = "block";
+    });
+
+    allLinks.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            desktopProjects.forEach(function(project) {
+                project.style.display = "block";
+            });
+            webProject.style.display = "block";
+        });
     });
 });
